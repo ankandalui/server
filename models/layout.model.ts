6a7b2898fd2b2d61b0fb5e83a45ff456e244eefe
ANKAN DALUI <143159255+ankandalui@users.noreply.github.com>
2024-07-19 19:14:39 +0530

    first commit

---
+++ models/layout.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface FaqItem extends Document {
+  question: string;
+  answer: string;
+}
+
+export interface Category extends Document {
+  title: string;
+}
+export interface Courseyear extends Document {
+  title: string;
+}
+
+export interface BannerImage extends Document {
+  public_id: string;
+  url: string;
+}
+
+interface Layout extends Document {
+  type: string;
+  faq: FaqItem[];
+  categories: Category[];
+  courseyear: Courseyear[];
+  banner: {
+    image: BannerImage;
+    title: string;
+    subTitle: string;
+  };
+}
+
+const faqSchema = new Schema<FaqItem>({
+  question: { type: String },
+  answer: { type: String },
+});
+
+const categorySchema = new Schema<Category>({
+  title: { type: String },
+});
+const courseyearSchema = new Schema<Courseyear>({
+  title: { type: String },
+});
+
+const bannerImageSchema = new Schema<BannerImage>({
+  public_id: { type: String },
+  url: { type: String },
+});
+
+const layoutSchema = new Schema<Layout>({
+  type: { type: String },
+  faq: [faqSchema],
+  categories: [categorySchema],
+  courseyear: [courseyearSchema],
+  banner: {
+    image: bannerImageSchema,
+    title: { type: String },
+    subTitle: { type: String },
+  },
+});
+
+const LayoutModel = model<Layout>("Layout", layoutSchema);
+
+export default LayoutModel;
